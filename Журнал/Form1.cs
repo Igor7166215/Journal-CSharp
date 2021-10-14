cd70cf58f87b8c646efa0e8e5859171c9e3f8f7c
Igor Shurinov <92313601+Igor7166215@users.noreply.github.com>
2021-10-14 10:47:49 +0300

    Journal

---
+++ Журнал/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Журнал
+{
+    public partial class Form1 : Form
+    {
+        public static string connectionstring = Properties.Settings.Default.ЖурналConnectionString;
+        public string SqlStr = "";
+        public OleDbConnection connection = new OleDbConnection(connectionstring);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private BindingSource bindingSource1;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (connection.State == ConnectionState.Closed)
+                connection.Open();
+            SqlStr = "SELECT " +
+                        "Ученики.[Фамилия]," +
+                        " Ученики.[Имя]," +
+                        " Ученики.[Отчество]," +
+                        " Дисциплины.[Название] AS [Дисциплина]," +
+                        " Оценки.[Оценки] " +
+                        "FROM Оценки, Ученики, Дисциплины " +
+                        "where Ученики.[Код] = Оценки.[Код ученика] and Дисциплины.[Код] = Оценки.[Код дисциплины]";
+            OleDbDataAdapter Adapter = new OleDbDataAdapter(SqlStr, connection);
+            DataTable Table = new DataTable();
+            Adapter.Fill(Table);
+            MainGrid.AutoGenerateColumns = true;
+            MainGrid.DataSource = Table;
+
+            bindingSource1 = new BindingSource();
+            bindingSource1.DataSource = Table;
+            //bindingNavigator1.BindingSource = bindingSource1;
+            MainGrid.DataSource = bindingSource1;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Добавить_оценку f2 = new Добавить_оценку();
+            f2.Show();
+        }
+
+        public void UpdBtn_Click(object sender, EventArgs e)
+        {
+            if (connection.State == ConnectionState.Closed)
+                connection.Open();
+            SqlStr = "SELECT " +
+                        "Ученики.[Фамилия]," +
+                        " Ученики.[Имя]," +
+                        " Ученики.[Отчество]," +
+                        " Дисциплины.[Название] AS [Дисциплина]," +
+                        " Оценки.[Оценки] " +
+                        "FROM Оценки, Ученики, Дисциплины " +
+                        "where Ученики.[Код] = Оценки.[Код ученика] and Дисциплины.[Код] = Оценки.[Код дисциплины]";
+            OleDbDataAdapter Adapter = new OleDbDataAdapter(SqlStr, connection);
+            DataTable Table = new DataTable();
+            Adapter.Fill(Table);
+            MainGrid.AutoGenerateColumns = true;
+            MainGrid.DataSource = Table;
+
+            bindingSource1 = new BindingSource();
+            bindingSource1.DataSource = Table;
+            MainGrid.DataSource = bindingSource1;
+
+        }
+    }
+}
